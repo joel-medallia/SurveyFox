c11a0baf573b4af2e880dd6ca979f710a4347df5
Joe Leung <joel@medallia.com>
2024-02-02 07:52:33 -0800

    Mon

--- Package.swift
+++ Package.swift
@@ -1,30 +1,34 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "medallia-sense360-ios-sdk",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
-        .library(name: "medallia-sense360-ios-sdk", targets: ["SenseQuinoaLib", "SwiftyJSON"])
+        .library(
+            name: "medallia-sense360-ios-sdk",
+            targets: ["SenseQuinoaLibWrapper"]
+        )
     ],
-
     dependencies: [
-        // Here we define our package's external dependencies
-        // and from where they can be fetched:
-        .package(name: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1")
+        .package(name: "SwiftyJSON", url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(name: "SwiftJWT", url: "https://github.com/Kitura/Swift-JWT.git", from: "4.0.0"),
     ],
-
     targets: [
         .binaryTarget(
             name: "SenseQuinoaLib",
-            path: "SenseQuinoaLib.xcframework"),
-        .binaryTarget(
-            name: "SwiftyJSON",
-            path: "SwiftyJSON.xcframework")
+            path: "SenseQuinoaLib.xcframework"
+        ),
+        .target(name: "SenseQuinoaLibWrapper",
+                dependencies: [
+                    .target(name: "SenseQuinoaLib", condition: .when(platforms: .some([.iOS]))),
+                    "SwiftyJSON",
+                    "SwiftJWT"
+                ],
+                path: "SenseQuinoaLibWrapper"
+        )
     ]
 )
-
