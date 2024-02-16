68b285e8b21ef7edc0a470d52a8c1b4405b48798
Joe Leung <joel@medallia.com>
2024-02-16 10:02:08 -0800

    For testing warnong and umbrella header

--- SenseQuinoaLib.xcframework/ios-arm64/SenseQuinoaLib.framework/Headers/QuinoaIOS.h
+++ SenseQuinoaLib.xcframework/ios-arm64/SenseQuinoaLib.framework/Headers/QuinoaIOS.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char QuinoaIOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <QuinoaIOS/PublicHeader.h>
 
-
+#import <SenseQuinoaLib/NSExceptionCatcher.h>
