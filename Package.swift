e5a9a94f14b9aad7602cb345f36b363cf60bdf51
Joe Leung <joel@medallia.com>
2024-02-21 10:28:23 -0800

    Changed the version to 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "medallia-sense360-ios-sdk",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(
